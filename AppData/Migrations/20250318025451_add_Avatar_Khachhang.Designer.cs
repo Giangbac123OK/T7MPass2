22c8a1732480899bd0749544e7f470714c1cc9c1
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2025-03-18 13:44:45 +0700

    update db them avatar

--- AppData/Migrations/20250318025451_add_Avatar_Khachhang.Designer.cs
+++ AppData/Migrations/20250318025451_add_Avatar_Khachhang.Designer.cs
@@ -4,6 +4,7 @@ using AppData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppData.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250318025451_add_Avatar_Khachhang")]
+    partial class add_Avatar_Khachhang
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
