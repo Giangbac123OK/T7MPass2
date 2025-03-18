2738c2d7c1e1fe9c659bab2453c4ff2c29153f00
Nguyễn Nhật Hưng <145339576+meou2k4@users.noreply.github.com>
2025-03-18 10:49:42 +0700

    update login

---
+++ AppData/DTO/RegisterUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Dto
+{
+	public class RegisterUserDTO
+	{
+		public string Ten { get; set; }
+		public string Sdt { get; set; }
+		public DateTime Ngaysinh { get; set; }
+		public string? Email { get; set; }
+		public string Diachi { get; set; }
+		public string? Password { get; set; }
+	}
+}
