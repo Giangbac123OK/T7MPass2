3c29d9be19e45221433e3ad2f3b3d96c63bca5dd
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2025-04-11 01:21:39 +0700

    ok

--- AppAPI/Controllers/NhanviensController.cs
+++ AppAPI/Controllers/NhanviensController.cs
@@ -216,7 +216,7 @@ namespace AppAPI.Controllers
         }
 
 
-        [HttpDelete("{id}")]
+        [HttpPut("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             try
@@ -224,15 +224,6 @@ namespace AppAPI.Controllers
                 var nhanvien = await _Service.GetNhanvienByIdAsync(id);
                 if (nhanvien == null)
                     return NotFound("Nhân viên không tồn tại.");
-
-                // Xoá ảnh nếu không phải ảnh mặc định
-                if (!string.IsNullOrEmpty(nhanvien.Avatar) && nhanvien.Avatar != "AnhNhanVien.png")
-                {
-                    var filePath = Path.Combine(_environment.WebRootPath, "picture", nhanvien.Avatar);
-                    if (System.IO.File.Exists(filePath))
-                        System.IO.File.Delete(filePath);
-                }
-
                 await _Service.DeleteNhanvienAsync(id);
                 return Ok(new { Success = true, Message = "Xóa thành công" });
             }
